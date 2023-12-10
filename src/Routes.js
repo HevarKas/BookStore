a000f1a31ea89b5fbbb8662c4c4e7f2f1ee892e7
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-12-10 13:03:35 +0300

    Updates

--- src/Routes.js
+++
@@ -1,8 +0,0 @@
-import { Routes, Route } from "react-router-dom";
-import Books from "./View/Books";
-const Main = () => (
-  <Routes>
-    <Route path="/" element={<Books />} />
-  </Routes>
-);
-export default Main;
--- src/redux/configureStore.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import BookSlice from "./books/books";
-
-export default configureStore({
-  reducer: {
-    books: BookSlice,
-  },
-});
